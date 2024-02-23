331b21eb8fe6e698536adc2e3c563894d5f204ab
Fernando-Carneiro27 <fernando.carneirosilva@outlook.com>
2024-02-23 18:07:17 -0300

    feat(props): exibe pedidos sem componentes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return (
+        <div className="container border rounded mt-2">
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="display-5 text-center">Seus pedidos</h1>
+                </div>
+            </div>
+
+            <div className="row">
+
+                <div className="col-sm-12 col-lg-4">
+                    <div className="card mb-2">
+                        <div className="card-header text-muted">
+                            20/04/2023
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className='d-flex align-items-center'>
+                                <div className="i fa-solid fa-book fa-2x"></div>
+                            </div>
+                            <div className='flex-grow-1 ms-2 border p-2'>
+                                <h4 className="text-center">Livro</h4>
+                                <p className='text-center'>Concrete Mathematics - Donald Knuth</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-sm-12 col-lg-4">
+                <div className="card mb-2">
+                        <div className="card-header text-muted">
+                            20/04/2023
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className='d-flex align-items-center'>
+                                <div className="i fa-solid fa-headphones fa-2x"></div>
+                            </div>
+                            <div className='flex-grow-1 ms-2 border p-2'>
+                                <h4 className="text-center">Fone</h4>
+                                <p className='text-center'>Fone de ouvido bluetooth, com carregador</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-sm-12 col-lg-4">
+                <div className="card mb-2">
+                        <div className="card-header text-muted">
+                            20/04/2023
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className='d-flex align-items-center'>
+                                <div className="i fa-solid fa-camera fa-2x"></div>
+                            </div>
+                            <div className='flex-grow-1 ms-2 border p-2'>
+                                <h4 className="text-center">Câmera</h4>
+                                <p className='text-center'>TekPix, com Lente EF-S 18-55mm Full HD 4K</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
