113668eeccc32de9a9ae158b8489bbd275978b9f
Fernando-Carneiro27 <fernando.carneirosilva@outlook.com>
2024-03-07 21:32:09 -0300

    refactor(ap04): fim da secao 2.8
    - Componente Cartao
    - Pedido deixa de utilizar classes de cartão do Bootstrap
    - Props de Pedido desestruturados

--- react-componentes-props/src/index.js
+++ react-componentes-props/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import Pedido from './Pedido'
+import Cartao from './Cartao'
 
 const App = () => {
     return (
-        <div className="container border rounded mt-2">
+        <div className="container border rounded mt-2 p-2">
 
             <div className="row">
                 <div className="col-12">
@@ -17,15 +18,35 @@ const App = () => {
             <div className="row">
 
                 <div className="col-sm-12 col-lg-4">
-                    <Pedido data="11/02/2021" icone="i fa-solid fa-book fa-2x" titulo="Livro" descricao="Concrete Mathematics - Donald Knuth" />
+                    <Cartao cabecalho="11/02/2021">
+                        <Pedido 
+                            icone="i fa-solid fa-book fa-2x" 
+                            titulo="Livro" 
+                            descricao="Concrete Mathematics - Donald Knuth" 
+                        />
+                    </Cartao>
+                        
                 </div>
 
                 <div className="col-sm-12 col-lg-4">
-                    <Pedido data="04/07/2022" icone="i fa-solid fa-headphones fa-2x" titulo="Fone" descricao="Fone de ouvido bluetooth, com carregador" />
+                    <Cartao cabecalho="04/07/2022">
+                        <Pedido 
+                            icone="i fa-solid fa-headphones fa-2x" 
+                            titulo="Fone" 
+                            descricao="Fone de ouvido bluetooth, com carregador" 
+                        />
+                    </Cartao>
+                    
                 </div>
 
                 <div className="col-sm-12 col-lg-4">
-                    <Pedido data="20/10/2023" icone="i fa-solid fa-camera fa-2x" titulo="Câmera" descricao="TekPix, com Lente EF-S 18-55mm Full HD 4K"/>
+                    <Cartao cabecalho="20/10/2023">
+                        <Pedido 
+                            icone="i fa-solid fa-camera fa-2x" 
+                            titulo="Câmera" 
+                            descricao="TekPix, com Lente EF-S 18-55mm Full HD 4K"
+                        />
+                    </Cartao>
                 </div>
 
             </div>
