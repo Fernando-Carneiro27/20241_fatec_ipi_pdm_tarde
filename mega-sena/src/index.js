d5f6504a9eb1b487025b7ad8980b57129ffdd312
Fernando-Carneiro27 <fernando.carneirosilva@outlook.com>
2024-03-22 20:40:43 -0300

    feat(ex05): finalizando o exercício 5
    - criando a função para sortear os 6 números da mega-sena
    - e melhorando a forma de apresentar os números sorteados

--- mega-sena/src/index.js
+++ mega-sena/src/index.js
@@ -1,32 +1,80 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {Table} from 'react-bootstrap'
 
 class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            numeros = []
+            numeros: []
         }
+    }
+
+    comecarJogo = () => {
+        const numeros = []
+        let n
+        for(let i = 0; i < 6; i++){
+            n = Math.floor(Math.random() * 60) + 1
+            numeros.push(n)
+        }
+
+        if (numeros.length !== new Set(numeros).size) {
+            this.comecarJogo();
+            return;
+        }
+        this.setState({numeros: numeros})
+    }
+
+    render(){
+        let e = this.state.numeros;
+        return (
+            
+            <div className="container border rounded mt-2">
+                <div className="row justify-content-center m-2">
+                    <div className="col-8 justify-content-center">
+                        
+                        <div 
+                            className="card-header text-muted container border rounded m-2 p-1"
+                            style={{backgroundColor: 'gainsboro'}}>
+                            <h3>Mega-Sena</h3>
+                        </div>
+                       
+                        <div className="card-body container border rounded m-2 p-2">
+
+                            <Table striped bordered style={{}}>
+                                <thead 
+                                    className="text-center p-2 mt-2"
+                                    style={{backgroundColor:'lightGreen', 
+                                            width:'40px',  
+                                            height:'50px', 
+                                            fontSize:'300%',
+                                            color:'darkgreen'}}>
+                                    <td style={{border: '2px solid'}}>{e[0] || '0'}</td>
+                                    <td style={{border: '2px solid'}}>{e[1] || '0'}</td>                                   
+                                    <td style={{border: '2px solid'}}>{e[2] || '0'}</td>                                   
+                                    <td style={{border: '2px solid'}}>{e[3] || '0'}</td>                                                                     
+                                    <td style={{border: '2px solid'}}>{e[4] || '0'}</td>                                                                
+                                    <td style={{border: '2px solid'}}>{e[5] || '0'}</td>                                
+                                </thead>
+                            </Table>
+
+                            <button 
+                                onClick={this.comecarJogo}      
+                                className="btn btn-outline-success mt-1 w-100"
+                                style={{fontSize:'20px'}}>
+                                    Gerar Jogo
+                            </button>
+                        </div>
+
+                    </div>
+                </div>       
+            </div>
+        )
     }
 }
 
-numerosRandom = () => {
-    let n
-    
-}
-
-render() {
-    return (
-        <div className="container rounded mt-2 p-2">
-            <div className="col-12">
-
-            </div>
-        </div>
-    )
-}
-
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.querySelector('#root')
 )
