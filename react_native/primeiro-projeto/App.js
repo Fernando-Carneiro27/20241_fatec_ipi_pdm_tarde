a31fda60fe2ae589ad19fcd3de729f8f765bffaa
Fernando-Carneiro27 <fernando.carneirosilva@outlook.com>
2024-05-09 17:32:06 -0300

    feat(ex001-rn): exercicio feito
    - soma dois números recebidos nos TextInput, após clicar no botão "SOMAR"

--- react_native/primeiro-projeto/App.js
+++ react_native/primeiro-projeto/App.js
@@ -1,10 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, StyleSheet, TextInput, View, Button, Text } from 'react-native';
 
 export default function App() {
+  const [state, setState] = useState({
+    n1: null,
+    n2: null,
+    result: null
+  });
+
+  const Valor1 = (valor) => {
+    setState({ ...state, n1: valor });
+  };  
+
+  const Valor2 = (valor) => {
+    setState({ ...state, n2: valor });
+  };
+
+  const soma = () => {
+    setState({ ...state, result: Number(state.n1) + Number(state.n2) });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello, React Native!</Text>
+      <TextInput
+        style={styles.texto}
+        onChangeText={text => Valor1(text)}
+        value={state.n1}
+      />
+      <TextInput
+        style={styles.texto}
+        onChangeText={text => Valor2(text)}
+        value={state.n2}
+      />
+      <Button
+        style={styles.button}
+        onPress={soma}
+        title="Somar"
+      />
+      <Text 
+        style={{fontFamily: 'arial', margin: 4}}>
+          <b>Resultado: {state.result}</b>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +53,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    alignContent: 'center',
+    margin: 4,
+    height: 40,
+    width: 200,
+    borderColor: 'darkgreen',
+    borderWidth: 2,
+    backgroundColor: 'palegreen',
+  },
+  button: {
+    margin: 4,
+    width: 200,
+    height: 40
+  }
 });
